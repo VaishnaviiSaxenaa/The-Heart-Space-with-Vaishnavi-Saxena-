56f6567115450195b77e35ffc3d04e6bbd00d041
Vaishnavi Saxena <theheartspacewithvs@gmail.com>
2026-06-26 12:02:41 +0000

    Remove Roadmap V2 (superseded by Calendar feature)

--- artifacts/heartspace/src/App.tsx
+++ artifacts/heartspace/src/App.tsx
@@ -14,7 +14,6 @@ import ExamSelect from "./pages/exam-select";
 import StudentDashboard from "./pages/student-dashboard";
 import CounsellorDashboard from "./pages/counsellor-dashboard";
 import MySessions from "./pages/my-sessions";
-import RoadmapV2Page from "./pages/roadmap-v2";
 import RevisionTracker from "./pages/revision-tracker";
 import NoteTracker from "./pages/note-tracker";
 import CounsellorSessionsPage from "./pages/counsellor-sessions";
@@ -133,9 +132,6 @@ function Router() {
         <Route path="/self-dashboard">
           <StudentRoute component={StudentDashboard} />
         </Route>
-        <Route path="/roadmap-v2">
-          <ProtectedRoute component={RoadmapV2Page} allowedRole="student" />
-        </Route>
         <Route path="/revision-tracker">
           <ProtectedRoute component={RevisionTracker} allowedRole="student" />
         </Route>
@@ -144,9 +140,6 @@ function Router() {
         </Route>
         <Route path="/counsellor-sessions">
           <ProtectedRoute component={CounsellorSessionsPage} allowedRole="counsellor" />
-        </Route>
-        <Route path="/roadmap-v2">
-          <ProtectedRoute component={RoadmapV2Page} allowedRole="student" />
         </Route>
         <Route path="/revision-tracker">
           <ProtectedRoute component={RevisionTracker} allowedRole="student" />
